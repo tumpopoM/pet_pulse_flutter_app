7298e5a9b7dc2f3a7a4f0b4dd50b1cbc5ad97dc4
tumpopoM <wijitrarattanason@gmail.com>
2026-03-28 20:52:11 +0700

    feat: add pet data model

---
+++ lib/models/pet.dart
@@ -0,0 +1,58 @@
+import 'package:uuid/uuid.dart';
+
+class Pet {
+  final String id;
+  final String name;
+  final String breed;
+  final DateTime birthDate;
+  final String? imagePath;
+  final bool isVaccinated;
+
+  Pet({
+    required this.id,
+    required this.name,
+    required this.breed,
+    required this.birthDate,
+    this.imagePath,
+    this.isVaccinated = false,
+  });
+
+  String get ageDisplay {
+    final now = DateTime.now();
+    int years = now.year - birthDate.year;
+    int months = now.month - birthDate.month;
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    if (years > 0) {
+      return '$years ปี $months เดือน';
+    } else {
+      return '$months เดือน';
+    }
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'breed': breed,
+      'birthDate': birthDate.toIso8601String(),
+      'imagePath': imagePath,
+      'isVaccinated': isVaccinated,
+    };
+  }
+
+  factory Pet.fromMap(Map<String, dynamic> map) {
+    return Pet(
+      id: map['id'],
+      name: map['name'],
+      breed: map['breed'],
+      birthDate: DateTime.parse(map['birthDate']),
+      imagePath: map['imagePath'],
+      isVaccinated: map['isVaccinated'] ?? false,
+    );
+  }
+}
